f025cb5d182018a4217fd68a11f27c972684f9f4
Hayyan55 <hayan_mez@hotmail.co.uk>
2022-07-01 00:30:50 +0200

    submit commit

---
+++ app/src/test/java/com/example/ikpmd/DatabaseHandlerTest.kt
@@ -0,0 +1,16 @@
+package com.example.ikpmd
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class DatabaseHandlerTest {
+
+    @Test
+    fun addWazaToTheDatabase() {
+    }
+
+    @Test
+    fun getJudoWazaList() {
+    }
+}
